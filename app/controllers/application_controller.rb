b9bf3877da64f55c4d455c385b369829189594c1
ui mizuhara <ui.mizuhara@gmail.com>
2018-03-28 17:39:31 +0900

    iroiro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def require_user_logged_in
+    unless user_signed_in?
+      redirect_to articles_path
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328060252) do
+ActiveRecord::Schema.define(version: 20180328082419) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
@@ -32,8 +32,11 @@ ActiveRecord::Schema.define(version: 20180328060252) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "article_id"
+    t.index ["article_id"], name: "index_users_on_article_id", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "users", "articles"
 end
